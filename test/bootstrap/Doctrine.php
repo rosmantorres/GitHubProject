bb1eae6e5d94f764feb12c3d4f669ff6d2b9869f
rt999 <rt999@ATI-003905.GRUPOCCO.COM>
2013-05-08 08:25:16 -0430

    Pruebas unitarias para el modelo Trabajo y para la logica

--- test/bootstrap/Doctrine.php
+++ test/bootstrap/Doctrine.php
@@ -1,10 +1,18 @@
 <?php
 
-include(dirname(__FILE__) . '/unit.php');
+include(dirname(__FILE__).'/unit.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'test', true);
 
 new sfDatabaseManager($configuration);
 
-Doctrine_Core::loadData(sfConfig::get('sf_test_dir') . '/fixtures');
+/*
+ * Datos de Prueba : para la BD de pruebas (jobeet_test), tenemos que llenarla
+ * con datos de pruebas, pero es necesario volver a cargar los datos cada vez 
+ * que ejecutamos las pruebas para conocer el estado inicial de la BD.
+ * La tarea doctrine:data-load internamente utiliza el método 
+ * Doctrine_Core::loadData() para cargar los datos y El objeto sfConfig puede
+ * ser utilizado para obtener la ruta completa de un sub-directorio del proyecto.
+ */
+Doctrine_Core::loadData(sfConfig::get('sf_test_dir').'/fixtures');
 ?>
--- test/unit/model/TrabajoTest.php
+++ test/unit/model/TrabajoTest.php
@@ -1,8 +1,10 @@
 <?php
+
 /*
  * Para ejecutar las pruebas:
  * $ php test/unit/model/TrabajoTest.php
- * $ php symfony test:unit 'model/Trabajo'
+ * $ php symfony test:unit model/Trabajo
+ * $ php symfony test:unit model/*
  */
 
 /*
@@ -13,7 +15,53 @@
 /*
  * http://ecapy.com/instalar-xdebug-en-windows-7-y-xampp/
  */
-$t = new lime_test(1);
 
-$t->pass('Esta prueba siempre pasa.');
+include(dirname(__FILE__) . '/../../bootstrap/Doctrine.php');
+
+$t = new lime_test(3);
+
+// 1era Prueba
+$t->comment('::getCompaniaSlug()');
+$trabajo = Doctrine_Core::getTable('Trabajo')->createQuery()->fetchOne();
+$t->is($trabajo->getCompaniaSlug(), Jobeet::slugear($trabajo->getCompania()), '::getCompaniaSlug() return el slug para la compania ' . $trabajo->getCompania());
+
+// 2da Prueba
+$t->comment('::save()');
+$job = create_job();
+$job->save();
+$expiresAt = date('Y-m-d', time() + 86400 * sfConfig::get('app_dias_activo'));
+$t->is($job->getDateTimeObject('expira_el')->format('Y-m-d'), $expiresAt, 
+        '::save() comprobando que el metodo save actualiza el campo expira_el');
+
+// 3era Prueba
+$job = create_job(array('expires_at' => '2008-08-08'));
+$job->save();
+$t->is($job->getDateTimeObject('expira_el')->format('Y-m-d'), '2013-06-07', '::save() comprobado si el save() funciona bien');
+
+function create_job($defaults = array())
+{
+  static $category = null;
+
+  if (is_null($category)) {
+    $category = Doctrine_Core::getTable('Categoria')
+            ->createQuery()
+            ->limit(1)
+            ->fetchOne();
+  }
+
+  $job = new Trabajo();
+  $job->fromArray(array_merge(array(
+      'categoria_id' => $category->getId(),
+      'compania' => 'Sensio Labs',
+      'posicion' => 'Senior Tester',
+      'localizacion' => 'Paris, France',
+      'descripcion' => 'Testing is fun',
+      'como_aplicar' => 'Send e-Mail',
+      'correo' => 'job@example.com',
+      'token' => rand(1111, 9999),
+      'esta_activado' => true,
+                  ), $defaults));
+  return $job;
+}
+
 ?>
