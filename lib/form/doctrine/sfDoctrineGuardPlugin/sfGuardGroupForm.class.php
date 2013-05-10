12d3dab3aee0b82b7b3e1a3638b1611a6f58f223
rt999 <rt999@ATI-003905.GRUPOCCO.COM>
2013-05-10 11:25:16 -0400

    reconstruyendo el modelo porque el nuevo plugin lo requiere: $ php symfony doctrine:build --all --and-load --no-confirmation

---
+++ lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardGroupForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sfGuardGroup form.
+ *
+ * @package    PROYECTO_PRUEBA
+ * @subpackage form
+ * @author     ROSMAN_TORRES
+ * @version    SVN: $Id: sfDoctrinePluginFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfGuardGroupForm extends PluginsfGuardGroupForm
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardPermissionForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sfGuardPermission form.
+ *
+ * @package    PROYECTO_PRUEBA
+ * @subpackage form
+ * @author     ROSMAN_TORRES
+ * @version    SVN: $Id: sfDoctrinePluginFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfGuardPermissionForm extends PluginsfGuardPermissionForm
+{
+  public function configure()
+  {
+  }
+}
